e6bab65a3db5e264a169c3439637e355cc18bae0
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2024-09-14 23:59:02 -0300

    atualizado implementação baseado nas novas dependencias

--- src/main/java/br/com/services/ArtistaService.java
+++ src/main/java/br/com/services/ArtistaService.java
@@ -1,28 +1,34 @@
 package br.com.services;
 
 import br.com.interfaces.model.IMusica;
+import br.com.interfaces.repository.IArtistaRepository;
+import br.com.interfaces.repository.IMusicaRepository;
 import br.com.interfaces.services.IArtistaService;
-import br.com.interfaces.services.IReproducaoService;
+import br.com.repositories.ArtistaRepository;
+import br.com.repositories.MusicaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class ArtistaService implements IArtistaService {
 
-    private ArtistaRepository artistaRepository;
+    private IArtistaRepository artistaRepository;
+    private IMusicaRepository  musicaRepository;
 
-    public ArtistaService(ArtistaRepository artistaRepository) {
-        this.artistaRepository = new ArtistaRepository();
+    public ArtistaService(IArtistaRepository artistaRepository) {
+        this.artistaRepository = ArtistaRepository.getArtistaRepository();
+        this.musicaRepository = MusicaRepository.getMusicaRepository();
     }
+
 
     @Override
     public Optional<List<IMusica>> getMusicasMaisTocadas(String nomeArtista) {
-        return this.artistaRepository.getMusicasMaisTocadas(nomeArtista, 5);
+        return this.musicaRepository.getMusicasMaisTocadas(nomeArtista, 5);
     }
 
     @Override
     public Optional<List<IMusica>> getMusica(String nomeArtista) {
-        return this.artistaRepository.getMusicas(nomeArtista);
+        return this.musicaRepository.getMusicas(nomeArtista);
     }
 
     @Override
@@ -30,11 +36,9 @@ public class ArtistaService implements IArtistaService {
         return this.artistaRepository.getBiografia(nomeArtista);
     }
 
-
-    //Esperar grupo rafa/gabriel
     @Override
-    public void atualizarEstatisticasReproducao(String s, IReproducaoService iReproducaoService) {
+    public void atualizarEstatisticasReproducao(IMusica musica) {
+        musica.getQtdVezesReproduzidas();
+        musica.incrementaContagemReproducao();
     }
-
-
 }
--- src/test/java/br/com/tests/ArtistaServiceTest.java
+++ src/test/java/br/com/tests/ArtistaServiceTest.java
@@ -1,20 +1,14 @@
 package br.com.tests;
 
-import br.com.services.ArtistaRepository;
-import br.com.services.ArtistaService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArtistaServiceTest {
 
-    private static ArtistaService artistaService;
 
     @BeforeAll
     public static void setUp() {
-        ArtistaRepository artistaRepository = new ArtistaRepository();
-        artistaService = new ArtistaService(artistaRepository);
     }
 
     @Test
