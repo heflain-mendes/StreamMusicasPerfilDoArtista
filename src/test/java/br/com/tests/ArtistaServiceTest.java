134920a6f1a90872e464d07f3f665aa648e09056
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2024-09-15 19:49:11 -0300

    refact tests

--- src/test/java/br/com/tests/ArtistaServiceTest.java
+++ src/test/java/br/com/tests/ArtistaServiceTest.java
@@ -7,13 +7,18 @@ import br.com.interfaces.model.IMusica;
 import br.com.interfaces.repository.IArtistaRepository;
 import br.com.interfaces.repository.IMusicaRepository;
 import br.com.model.Artista;
+import br.com.model.Musica;
 import br.com.services.ArtistaService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,15 +34,30 @@ public class ArtistaServiceTest {
     @InjectMocks
     private ArtistaService artistaService;
 
+    private static Artista artistaMocked;
+    private static Musica musicaMocked;
+
+    @BeforeAll
+    public static void setUpBeforeClass() throws Exception {
+        artistaMocked = new Artista("Hiago Moreira", "Um cara legal !");
+        musicaMocked = new Musica("Titulo", artistaMocked.getNome(), "Rock", 22 );
+    }
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         this.artistaService = new ArtistaService(artistaRepository, musicaRepository);
     }
 
+    @AfterEach
+    public void tearDown() {
+        //clean mocks after tests
+        Mockito.reset(artistaRepository, musicaRepository);
+    }
+
     @Test
     public void verificaRetornoSeNaoExistirArtista(){
-        //Given: Artista mockado no repositorio
+        //Given
         when(artistaRepository.getBiografia("Hiago Moreira"))
                 .thenReturn(Optional.empty());
 
@@ -48,12 +68,12 @@ public class ArtistaServiceTest {
                 .thenReturn(Optional.empty());
 
 
-        //When : utilização do serviço buscando pelo nome do artista
+        //When
         Optional<String> biografia = artistaService.getBiografia("Hiago Moreira");
         Optional<List<IMusica>> musicas = artistaService.getMusica("Hiago Moreira");
         Optional<List<IMusica>> musicasMaisTocadas = artistaService.getMusicasMaisTocadas("Hiago Moreira");
 
-        //Then : verifica a chamada no artista service
+        //Then
         assertFalse(biografia.isPresent());
         assertFalse(musicas.isPresent());
         assertFalse(musicasMaisTocadas.isPresent());
@@ -66,15 +86,14 @@ public class ArtistaServiceTest {
 
     @Test
     public void verificaRetornoBibliografiaTest(){
-        //Given: Artista mockado no repositorio
-        Artista artistaMocked = new Artista("Hiago Moreira", "Um cara legal !");
+        //Given
         when(artistaRepository.getBiografia("Hiago Moreira"))
                 .thenReturn(Optional.ofNullable(artistaMocked.getBiografia()));
 
-        //When : utilização do serviço buscando pelo nome do artista
+        //When
         Optional<String> biografia = artistaService.getBiografia("Hiago Moreira");
 
-        //Then : verifica a chamada no artista service
+        //Then
         assertTrue(biografia.isPresent());
         assertEquals("Um cara legal !", biografia.get());
         verify(artistaRepository, times(1)).getBiografia("Hiago Moreira");
@@ -83,41 +102,42 @@ public class ArtistaServiceTest {
 
     @Test
     public void verificaRetornoMusicas(){
-        //Given: Artista mockado no repositorio
-        when(musicaRepository.getMusicas("Hiago Moreira"))
-                .thenReturn(Optional.ofNullable(null));
+        //Given
+        List<IMusica> musicaRetono = new ArrayList<IMusica>();
+        musicaRetono.add(musicaMocked);
+        when(musicaRepository.getMusicasMaisTocadas("Hiago Moreira", 5))
+                .thenReturn(Optional.ofNullable(musicaRetono));
 
-        //When : utilização do serviço buscando pelo nome do artista
+        //When
         Optional<String> biografia = artistaService.getBiografia("Hiago Moreira");
 
-        //Then : verifica a chamada no artista service
+        //Then
         assertFalse(biografia.isPresent());
         verify(artistaRepository, times(1)).getBiografia("Hiago Moreira");
     }
 
     @Test
     public void verificaRetornoMusicasMaisTocadas(){
-        //Given: Artista mockado no repositorio
+        //Given
+        List<IMusica> musicaRetono = new ArrayList<IMusica>();
+        musicaRetono.add(musicaMocked);
         when(musicaRepository.getMusicasMaisTocadas("Hiago Moreira", 5))
-                .thenReturn(Optional.ofNullable(null));
+                .thenReturn(Optional.ofNullable(musicaRetono));
 
-        //When : utilização do serviço buscando pelo nome do artista
+        //When
         Optional<List<IMusica>> musicasMaisTocadas = artistaService.getMusicasMaisTocadas("Hiago Moreira");
 
-        //Then : verifica a chamada no artista service
+        //Then
         assertFalse(musicasMaisTocadas.isPresent());
         verify(musicaRepository, times(1)).getMusicasMaisTocadas("Hiago Moreira", 5);
     }
 
     @Test
     public void verificaAtualizacaoReproducaoTest(){
-        //Given: Musica mockada
-        IMusica musicaMocked = mock(IMusica.class);
-
-        //When : utilização do serviço buscando pelo nome do artista
+        //When
         artistaService.atualizarEstatisticasReproducao(musicaMocked);
 
-        //Then : verifica a chamada no artista service
+        //Then
         verify(musicaRepository, times(1)).atualizarEstatisticasReproducao(musicaMocked);
     }
 
